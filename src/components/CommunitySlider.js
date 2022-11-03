cec4fbdc395d02f52b9e38c6303e43300edf37e9
Yevgen Kostenko <jekcort969@gmail.com>
2022-11-03 15:04:53 +0200

    add new component

--- src/components/CommunitySlider.js
+++ src/components/CommunitySlider.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import {Swiper,SwiperSlide} from "swiper/react";
 
-const CommunitySlider = () => {
-  return <div>CommunitySlider</div>;
+import 'swiper/css'
+import 'swiper/css/navigation'
+
+
+
+const CommunitySlider = ({testimonials}) => {
+  return <Swiper
+      slidesPerView={3}
+      spaceBetween={32}
+      centeredSlides={'auto'}
+      grabCursor={true}
+      modules={[]}
+  >
+    {testimonials.map((testimonials, idx)=>{
+      const {image, name, message} = testimonials
+      return <SwiperSlide key={idx}>
+        <div className="relative">
+          <div>
+            <img src={image} alt="testimonials image"/>
+          </div>
+
+          {/******-----message and name-----******/}
+<div className='absolute bottom-[30px] text-white p-[20px] text-center'>
+  <div className='mb-8 italic text-lg font-light'>{message}</div>
+  <div className='flex items-center justify-center gap-x-[3px]'>
+
+    <span className='text-[30px] text-primary-200 font-bold'>~</span>
+
+    <div className='text-[20px] font-bold'>{name}</div>
+  </div>
+</div>
+        </div>
+        </SwiperSlide>
+    })}
+  </Swiper>;
 };
 
 export default CommunitySlider;
